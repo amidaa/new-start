c3b10ce845eaa85ab27468d47ddc92ad22e36573
amidaa <15583291353@163.com>
2018-11-11 18:42:26 +0800

    update datebase

---
+++ app/src/main/java/zhang/feng/com/eatwhat/Users.java
@@ -0,0 +1,42 @@
+package zhang.feng.com.eatwhat;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Users extends LitePalSupport{
+    private String username;
+    private String password;
+    private int age;
+    private String sex;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
